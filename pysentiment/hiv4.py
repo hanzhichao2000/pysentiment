936b0f8602a5f520c1ed5a11802cf761f8fcb007
Zhichao <hanzhichao2000@foxmail.com>
2018-10-27 16:20:38 +0800

    added pandas query

--- pysentiment/hiv4.py
+++ pysentiment/hiv4.py
@@ -3,26 +3,16 @@ from pysentiment.base import STATIC_PATH, BaseDict
 
 
 class HIV4(BaseDict):
-    '''
+    """
     Dictionary class for Harvard IV-4. 
     See also http://www.wjh.harvard.edu/~inquirer/
     
     The terms for the dictionary are stemmed by the default tokenizer.
-    '''
+    """
     
     PATH = '%s/HIV-4.csv' % STATIC_PATH
     
     def init_dict(self):
-        data = pd.read_csv(self.PATH)
-        for category in ['Positiv', 'Negativ']:
-            terms = data['Entry'][data[category] == category]
-            if category == 'Positiv':
-                for t in terms:
-                    t = self.tokenize(t)
-                    if len(t) > 0:
-                        self._posset.add(t[0])
-            else:
-                for t in terms:
-                    t = self.tokenize(t)
-                    if len(t) > 0:
-                        self._negset.add(t[0])
+        data = pd.read_csv(self.PATH, dtype='category')
+        self._posset = set(data.query('Positiv == "Positiv"')['Entry'].apply(self.tokenize_first).dropna())
+        self._negset = set(data.query('Negativ == "Negativ"')['Entry'].apply(self.tokenize_first).dropna())
--- pysentiment/lm.py
+++ pysentiment/lm.py
@@ -3,28 +3,18 @@ from pysentiment.base import STATIC_PATH, BaseDict
 
 
 class LM(BaseDict):
-    '''
+    """
     Dictionary class for
     Loughran and McDonald Financial Sentiment Dictionaries.
     
     See also https://www3.nd.edu/~mcdonald/Word_Lists.html
     
     The terms for the dictionary are stemmed by the default tokenizer.
-    '''
+    """
     
     PATH = '%s/LM.csv' % STATIC_PATH
     
     def init_dict(self):
         data = pd.read_csv(self.PATH)
-        for category in ['Positive', 'Negative']:
-            terms = data['Word'][data[category] > 0]
-            if category == 'Positive':
-                for t in terms:
-                    t = self.tokenize(t)
-                    if len(t) > 0:
-                        self._posset.add(t[0])
-            else:
-                for t in terms:
-                    t = self.tokenize(t)
-                    if len(t) > 0:
-                        self._negset.add(t[0])
+        self._posset = set(data.query('Positive > 0')['Word'].apply(self.tokenize_first).dropna())
+        self._negset = set(data.query('Negative > 0')['Word'].apply(self.tokenize_first).dropna())
