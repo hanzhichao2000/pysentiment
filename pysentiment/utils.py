cd43fd1221b74f491558f16b013f5a370a9c3e22
Zhichao <hanzhichao2000@foxmail.com>
2018-10-27 16:20:59 +0800

    minor on format

--- pysentiment/utils.py
+++ pysentiment/utils.py
@@ -1,38 +1,35 @@
-'''
+"""
 This module contains methods to tokenize sentences.
-'''
+"""
 import abc
 import re
 import nltk
 
 
-class BaseTokenizer(object):
-    '''
+class BaseTokenizer(object, metaclass=abc.ABCMeta):
+    """
     An abstract class for tokenize text.
-    '''
-    
-    
-    __metaclass__ = abc.ABCMeta
-    
+    """
+
     @abc.abstractmethod
     def tokenize(self, text):
-        '''Return tokenized temrs.
+        """Return tokenized temrs.
         
         :type text: str
         
         :returns: list 
-        '''
+        """
         pass
 
 
 class Tokenizer(BaseTokenizer):
-    '''
+    """
     The default tokenizer for ``pysentiment``, which only takes care of words made up of ``[a-z]+``.
     The output of the tokenizer is stemmed by ``nltk.PorterStemmer``. 
     
     The stoplist from https://www3.nd.edu/~mcdonald/Word_Lists.html is included in this
     tokenizer. Any word in the stoplist will be excluded from the output.
-    '''
+    """
     
     def __init__(self):
         self._stemmer = nltk.PorterStemmer()
